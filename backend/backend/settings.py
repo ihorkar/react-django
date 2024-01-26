4792cf5740209d5a2a607421102b6973559d3f03
ihorkar <ihorkarpiuk0709@gmail.com>
2024-01-26 10:01:17 +0200

    completed todo functionality

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,6 +53,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
+
+# CORS_ORIGIN_ALLOW_ALL = True
+
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
